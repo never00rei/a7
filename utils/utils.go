3e16f84f2ce913977ffb99672ead7aeb836bf504
James Washington <jwashington104.jw@gmail.com>
2024-04-01 11:44:13 +0100

    feat(config): setup basic configuration system

--- utils/utils.go
+++ utils/utils.go
@@ -66,3 +66,17 @@ func RandomStringFromSlice(slice []string) string {
 
 	return str
 }
+
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+
+	if err == nil {
+		return true, nil
+	}
+
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+
+	return false, err
+}
