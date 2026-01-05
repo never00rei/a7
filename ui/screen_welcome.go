3ef19cb9779c4f6028f395c1fa227f604571bb06
never00rei <jwashington104.jw@gmail.com>
2026-01-05 22:10:50 +0000

    chore(welcome): Updated welcome message.

--- ui/screen_welcome.go
+++ ui/screen_welcome.go
@@ -2,8 +2,8 @@ package ui
 
 func (m AppModel) viewWelcome() string {
 	bodyText := "A7 is a calm space for daily journaling.\n" +
-		"Write in plain Markdown and keep journals on your machine.\n" +
-		"Let's take 60 seconds to set things up.\n\n" +
+		"Write in plain Markdown and keep journals on your machine.\n\n" +
+		"So, let's take 60 seconds to set things up.\n\n" +
 		"No accounts.\nNo cloud.\nJust you and your journal."
 
 	pane := m.titledPaneWithWidth("Welcome to your Terminal Journal", bodyText, m.primaryPaneWidth())
